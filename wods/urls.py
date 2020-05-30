1e6c541a6a93e19990df985fcce59be689d3a821
Florian Fusseder <fussederflorian@gmail.com>
2020-05-30 19:13:45 +0200

    Added rudimental html structure

--- wods/urls.py
+++ wods/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wods'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:workout_id>/', views.detail, name='detail'),
+    path('create/<int:workout_id/>', views.create, name='create'),
 ]
--- wods/views.py
+++ wods/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import logging
+
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+
+from wods.models import Workout
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    wods = Workout.objects.order_by('-pub_date')[:25]
+    return render(request, "wods/index.html", {'latest_workouts_list': wods})
+
+
+def detail(request, workout_id):
+    logger.info(f"Get workout with id {workout_id}")
+    workout = get_object_or_404(Workout, id=workout_id)
+    return render(request, "wods/detail.html", {'workout': workout})
+
+
+def create(request, workout_id):
+    return HttpResponse(f"You're voting on workout {workout_id}.")
