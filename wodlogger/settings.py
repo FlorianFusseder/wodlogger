237d36d96eed07fe02c9ee228e048a8220f07457
Florian Fusseder <fussederflorian@gmail.com>
2020-05-30 16:28:14 +0200

    Added settings local from django_heroku

--- wodlogger/settings.py
+++ wodlogger/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -115,4 +117,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
