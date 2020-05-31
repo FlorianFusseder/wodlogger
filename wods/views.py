af868c594e0c704097d8710539844d6d9fb9db82
Florian Fusseder <fussederflorian@gmail.com>
2020-05-31 14:35:01 +0200

    Changed views to use generics

--- wods/views.py
+++ wods/views.py
@@ -3,22 +3,23 @@ import logging
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.views import generic
 
 from wods.models import Workout
 
 logger = logging.getLogger(__name__)
 
 
-def index(request):
-    wods = Workout.objects.order_by('-pub_date')[:25]
-    return render(request, "wods/index.html", {'latest_workouts_list': wods})
+class IndexView(generic.ListView):
+    context_object_name = 'workout_list'
+
+    def get_queryset(self):
+        return Workout.objects.order_by('-pub_date')[:25]
 
 
-def detail(request, workout_id):
-    logger.info(f"Get workout with id {workout_id}")
-    workout = get_object_or_404(Workout, id=workout_id)
-    return render(request, "wods/detail.html", {'workout': workout})
+class DetailView(generic.DetailView):
+    model = Workout
 
 
-def create(request, workout_id):
-    return HttpResponse(f"You're voting on workout {workout_id}.")
+class CreateView(generic.CreateView):
+    model = Workout
